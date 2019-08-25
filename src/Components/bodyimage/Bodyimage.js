ba60737f3bf53ed7aacf4ac84c5e26fdbea93019
Monika Nawrocka <monikanawrocka@onet.pl>
2019-08-25 20:06:13 +0200

    background image

--- src/Components/bodyimage/Bodyimage.js
+++ src/Components/bodyimage/Bodyimage.js
@@ -10,46 +10,55 @@ class Bodyimage extends Component {
     class: ["pizzabackground", 'burgerbackground', 'drinksbackground', 'cookiesbackground']
   }
 
-  changeBlogPicForwards() {
-    let i = this.state.index;
-    if (i === this.state.class.length - 1) {
-        i = 0;
-    } else {
-        i = i + 1;
-    }
-    this.setState({index: i});
+changeBlogPicForwards() {
+  let i = this.state.index;
+  if (i === this.state.class.length - 1) {
+      i = 0;
+  } else {
+      i = i + 1;
+  }
+  this.setState({index: i});
 }
 
 changeBlogPicBackwards() {
-    let i = this.state.index;
-    if (i === 0) {
-        i = this.state.class.length - 1;
-    } else {
-        i = i - 1;
-    }
-    this.setState({index: i});
+  let i = this.state.index;
+  if (i === 0) {
+      i = this.state.class.length - 1;
+  } else {
+      i = i - 1;
+  }
+  this.setState({index: i});
 }
 
   render() {
-   let blogCurrentPic = this.state.class[this.state.index]
+   let blogCurrentPic = this.state.class[this.state.index];
 
-   if(blogCurrentPic) {
-     console.log(blogCurrentPic);
+   let blogCurrentTitle = null;
+   let i = this.state.index;
+
+   if( i === 0) {
+     blogCurrentTitle = "Pizza and more"
+   }else if( i === 1){
+     blogCurrentTitle = "Delicious burger"
+   } else if (i === 2){
+      blogCurrentTitle = "Refresh drink"
+   }else {
+    blogCurrentTitle = "Chocolaty cookies"
    }
 
     return(
       <Aux>
         <div className="background-image">
             <div className={`image-holder ${blogCurrentPic}`}>
-             <p>mkk</p>
+              <p className="title-background"> {blogCurrentTitle} </p>
             </div>
             <div className="blog-name">
-                <div className="left-arrow-action arrow-icons">
-                    <i onClick={(e) => this.changeBlogPicForwards(e)} className="fa fa-arrow-circle-left fa-2x" aria-hidden="false"></i>
-                </div>
-                <div className="right-arrow-action arrow-icons">
-                    <i onClick={(e) => this.changeBlogPicBackwards(e)} className="fa fas fa-arrow-circle-right fa-2x fa-2x" aria-hidden="false"></i>
-                </div>
+              <div className="left-arrow-action arrow-icons">
+                  <i onClick={(e) => this.changeBlogPicBackwards(e)} className="fa fa-angle-left fa-4x" aria-hidden="false"></i>
+              </div>
+              <div className="right-arrow-action arrow-icons">
+                  <i onClick={(e) => this.changeBlogPicForwards(e)} className="fa fas fa-angle-right fa-4x" aria-hidden="false"></i>
+              </div>
             </div>
         </div>
      </Aux>
