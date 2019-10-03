feb7b859c30775dc90652c97ec2123cc53697a27
Monika Nawrocka <monikanawrocka@onet.pl>
2019-10-03 18:38:20 +0200

    minDate and maxDate

--- src/Components/Booking/Booking.js
+++ src/Components/Booking/Booking.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import Calendar from 'react-calendar'
 
-const Booking = () => (
-  <Calendar />
-)
+var date = new Date();
+var three = new Date(new Date().setDate(new Date().getDate() + 40));
+console.log(three)
+
+
+const Booking = () => {
+  return(
+    <div className="calendar">
+     <p>  jjnjsns jkjsdnjs </p>
+     <Calendar
+       calendarType = "US"
+       minDate = {date}
+       maxDate = {three}
+     />
+    </div>
+ )
+}
 
 export default Booking;
