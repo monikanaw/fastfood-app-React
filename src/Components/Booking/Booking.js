ecb3326ff5dd9a86504644080e5cd60f411bb6e5
Monika Nawrocka <monikanawrocka@onet.pl>
2019-10-03 22:08:57 +0200

    props to calendar

--- src/Components/Booking/Booking.js
+++ src/Components/Booking/Booking.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 import Calendar from 'react-calendar'
+import './Booking.css';
 
 var date = new Date();
-var three = new Date(new Date().setDate(new Date().getDate() + 40));
+var three = new Date(new Date().setDate(new Date().getDate() + 15));
 console.log(three)
 
 
 const Booking = () => {
+
+  const onDay = (value) => {
+    console.log(value)
+  }
+
   return(
     <div className="calendar">
-     <p>  jjnjsns jkjsdnjs </p>
+     <p className="calendar-title"> Choose a day, hour and a table you want to book </p>
+     <p> (Our calendar is activ only for 15 days in advance) </p>
      <Calendar
        calendarType = "US"
        minDate = {date}
        maxDate = {three}
+       value={date}
+       onClickDay={(value) => onDay(value)}
      />
     </div>
  )
