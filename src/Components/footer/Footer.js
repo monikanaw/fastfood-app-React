ec25ca246f4d9d745b9e32c66ffde2d6ba755758
Monika Nawrocka <monikanawrocka@onet.pl>
2019-10-18 23:54:35 +0200

    footer

--- src/Components/footer/Footer.js
+++ src/Components/footer/Footer.js
@@ -6,13 +6,12 @@ import phone from '../images/phone.png'
 
 const Footer = () => (
   <div className="footer">
-     <div>
+     <div className="name-address">
       <p> Pizza Italia </p>
       <p> Krakowska 56 </p>
       <p> 51-111 Wroclaw </p>
      </div>
-     < Logo />
-     <div>
+     <div className="contact">
        <img src={phone} alt={"phone"}/>
        <p> phone: 111-222-333 </p>
        <img src={email} alt={"email"}/>
